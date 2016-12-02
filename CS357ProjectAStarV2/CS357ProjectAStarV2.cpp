d8312369aea9281e23164a8e8326636e1c03f980
Alex Reinemann <Reineman18@up.edu>
2016-12-02 10:36:23 -0800

    added initial io

--- CS357ProjectAStarV2/CS357ProjectAStarV2.cpp
+++ CS357ProjectAStarV2/CS357ProjectAStarV2.cpp
@@ -2,10 +2,69 @@
 //
 
 #include "stdafx.h"
+#include <string>
+#include <iostream>
+#include <fstream>
+using namespace std;
 
+struct nfa
+{
+	string* sStates;
+	char*	cAlfa;
+	int		iStart;
+	int*	iFinal;
+};
 
 int main()
 {
+	string		sFileName,
+				sLine;
+	ifstream	inputFile;
+	nfa*		myNFA		= new nfa;
+	int			i;
+
+
+	cout	<< "\n Please enter the file location for the NFA or DFA you wish to convert to the formate A*:\n";
+	cin		>> sFileName;
+
+	inputFile.open(sFileName);
+
+	for (i = 0; i < 5; i++)
+	{
+		getline(inputFile, sLine);
+
+		switch(i)
+		{
+			case 0:
+			{
+				break;
+			}
+			case 1:
+			{
+				break;
+			}
+			case 2:
+			{
+				break;
+			}
+			case 3:
+			{
+				break;
+			}
+			case 4:
+			{
+				break;
+			}
+			default:
+			{
+				break;
+			}
+		}
+	}
+
+	inputFile.close();
+
+	delete	myNFA;
     return 0;
 }
 
