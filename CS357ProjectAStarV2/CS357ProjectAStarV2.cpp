6e1232d23265bcd6005b00f3c2002dfc9dad23ae
Alex Reinemann <Reineman18@up.edu>
2016-12-02 13:46:35 -0800

    Added comments to code

--- CS357ProjectAStarV2/CS357ProjectAStarV2.cpp
+++ CS357ProjectAStarV2/CS357ProjectAStarV2.cpp
@@ -22,29 +22,33 @@ struct nfa
 
 int main()
 {
+	//Variables
 	string		sFileName;
 	ifstream	inputFile;
-	nfa			myNFA;
-	int			i;
+	nfa			myNFA;		//NFA used in program
+	int			i;			//counter
 
-
+	//get the file's location
 	cout	<< "\n Please enter the file location for the NFA or DFA you wish to convert to the formate A*:\n";
 	cin		>> sFileName;
 
+	//open the file
 	inputFile.open(sFileName);
 
+	//loop through each line in the file: there should be 5 lines
 	for (i = 0; i < 5; i++)
 	{
-		int j,
+		int j,			//secondary counter
 			len;
-		string	line;
-		string*	temp;
+		string	line;	//line from the file
+		string*	temp;	//temporary string array
 
 		
-
+		//get the line and stor both it and its length
 		getline(inputFile, line);
 		len = line.size();
 		
+		//set up string array: larger than necessary
 		temp = new string[len];
 
 		switch(i)
@@ -52,58 +56,67 @@ int main()
 			case 0://finds the set of states
 			{
 				int y = 0;
+				//loop through characters in the line
 				for (j = 0; j < len; j++)
 				{
 					if (line[j] == ',')
-					{
+					{//state divison
 						y++;
 					}
 					else if (line[j] != '\n')
-					{
+					{//add char to state's placement in temp string
 						temp[y] = temp[y] + line[j];
 					}
 				}
 
+				//add one for new state
 				y++;
 
+				//set up the NFA's states to the proper number
 				myNFA.sStates = new string[y];
+				myNFA.iNumStates = y;
 
+				//loop through temp array moving states to NFA
 				for (j = 0; j < y; j++)
 				{
 					myNFA.sStates[j] = temp[j];
 				}
 
+				//add new state to NFA
 				myNFA.sStates[y - 1] = "Qnewstart";
 
-				myNFA.iNumStates = y;
 
 				break;
 			}
 			case 1://finds the alphabet of the NFA
 			{
 				int y = 0;
+
+				//new array to hold alfa characters
 				char* cTemp = new char[len];
+
+				//loop through line and extract charactors
 				for (j = 0; j < len; j++)
 				{
 					if (line[j] == ',')
-					{
+					{//charactor divison found
 						y++;
 					}
 					else if (line[j] != '\n')
-					{
+					{//put character in temp
 						cTemp[y] = line[j];
 					}
 				}
 
+				//initialize the NFA's array of alfa chars
 				myNFA.cAlfa = new char[y];
-
-				for (j = 0; j < y; j++)
-				{
-					myNFA.cAlfa[j] = cTemp[j];
-				}
-
 				myNFA.iNumAlfa = y;
 
+				for (j = 0; j < y; j++)
+				{//add chars to NFA
+					myNFA.cAlfa[j] = cTemp[j];
+				}
+				
 				break;
 			}
 			case 2://finds the transitions of the NFA
@@ -114,25 +127,26 @@ int main()
 			{
 				string tempState;
 				for (j = 0; j < len; j++)
-				{
+				{//aquire the starting states name
 					if ((line[j] != ',') && (line[j] != '\n'))
 					{
 						tempState = tempState + line[j];
 					}
 				}
 
+				//find the address of the start state
 				myNFA.iStart = -1;
 				for (j = 0; j < myNFA.iNumStates; j++)
 				{
 					if (tempState.compare(myNFA.sStates[j]) == 0)
-					{
+					{//address found
 						myNFA.iStart = j;
 						j = myNFA.iNumStates;
 					}
 				}
 
 				if (myNFA.iStart == -1)
-				{
+				{//determin if the address was not found
 					cout << "\n Start State is not in the set of states!\n";
 				}
 
@@ -141,10 +155,11 @@ int main()
 			case 4://finds the accept states of the NFA.
 			{
 				int y = 0;
+				//loop through the line and get the names of the finish states
 				for (j = 0; j < len; j++)
 				{
 					if (line[j] == ',')
-					{
+					{//state division found
 						y++;
 					}
 					else if (line[j] != '\n')
@@ -153,17 +168,23 @@ int main()
 					}
 				}
 
+				//initialize the NFA's array of final state addresses
 				myNFA.iNumFinalStates = y;
 				myNFA.iFinal = new int[y];
-				
-				int m;
-
 				for (j = 0; j < y; j++)
 				{
+					myNFA.iFinal[j] = -1;
+				}
+
+				
+				int m;		//tertiary counter
+
+				for (j = 0; j < y; j++)
+				{//loop through final states
 					for (m = 0; j < myNFA.iNumStates; m++)
-					{
+					{//loop through all states
 						if (temp[j].compare(myNFA.sStates[m]) == 0)
-						{
+						{//save the address of the states
 							myNFA.iFinal[j] = m;
 							m = myNFA.iNumStates;
 						}
@@ -175,6 +196,7 @@ int main()
 		}
 	}
 
+	//close the file
 	inputFile.close();
 
     return 0;
